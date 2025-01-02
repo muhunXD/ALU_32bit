e210830b5c08afac434ebb1f1ce394f8679e6af0
muhunXD <muhun2548@gmail.com>
2025-01-03 04:51:25 +0700

    Co-exist A,B_invert

--- ALU101.vhd
+++ ALU101.vhd
@@ -16,17 +16,22 @@ end ALU101;
 
 architecture Structural of ALU101 is
     signal Carry : std_logic_vector(31 downto 0);
+	 signal Ainvert_control , Binvert_control  : std_logic;
     signal Less  : std_logic;
     signal Internal_Result : std_logic_vector(31 downto 0);
 begin
     -- Instantiate 1-bit ALU for LSB
+	 Ainvert_control <= Operation(3) xor Ainvert;
+	 Binvert_control <= Operation(2) xor Binvert;
+	 
+	 
     ALU_LSB: entity work.ALU_1bit
         port map (
             A => A(0),
             B => B(0),
             CarryIn => Operation(2), -- CarryIn corresponds to ADD (0) or SUB (1)
-            Ainvert => Operation(3),
-            Binvert => Operation(2),
+            Ainvert => Ainvert_control,
+            Binvert => Binvert_control,
             Operation => Operation,
             Less => Less,            -- Use Less signal from MSB
             Result => Internal_Result(0),
@@ -42,8 +47,8 @@ begin
                 A => A(i),
                 B => B(i),
                 CarryIn => Carry(i),
-                Ainvert => Operation(3),
-                Binvert => Operation(2),
+                Ainvert => Ainvert_control,
+                Binvert => Binvert_control,
                 Operation => Operation,
                 Less => '0',
                 Result => Internal_Result(i),
@@ -59,8 +64,8 @@ begin
             A => A(31),
             B => B(31),
             CarryIn => Carry(31),
-            Ainvert => Operation(3),
-            Binvert => Operation(2),
+            Ainvert => Ainvert_control,
+            Binvert => Operation(2) xor Binvert,
             Operation => Operation,
             Less => '0',
             Result => Internal_Result(31),
